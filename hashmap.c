b1390cf2a8cf3bba18fdcc8181b040814b950efa
Andres Garcia Muñoz <163463179+littlenegrito@users.noreply.github.com>
2024-04-25 19:18:06 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -99,7 +99,12 @@ Pair * searchMap(HashMap * map,  char * key) {
 
 
 Pair * firstMap(HashMap * map) {
-
+  for (int i = 0; i < map->capacity; i++) { // recorrer mapa
+      if (map->buckets[i] != NULL && map->buckets[i]->key != NULL) {
+          map->current = i;
+          return map->buckets[i];
+      }
+  }
     return NULL;
 }
 
